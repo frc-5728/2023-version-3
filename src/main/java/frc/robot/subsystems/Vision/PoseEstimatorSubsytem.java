2518ce5f409d562b4fa05144b87c7f27701cf878
Team 5728 <frc.robotics.5728@gmail.com>
2023-02-10 16:37:51 -0800

    getrange

--- src/main/java/frc/robot/subsystems/Vision/PoseEstimatorSubsytem.java
+++ src/main/java/frc/robot/subsystems/Vision/PoseEstimatorSubsytem.java
@@ -10,19 +10,29 @@ import org.photonvision.PhotonCamera;
 import org.photonvision.PhotonUtils;
 import org.photonvision.common.hardware.VisionLEDMode;
 
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class PoseEstimatorSubsytem extends SubsystemBase {
 
-    private double yawVal=0;
-    private double pitchVal=0;
-    private double skewVal=0;
-    private double areaVal=0;
+    // Constants such as camera and target height stored.
+    final double CAMERA_HEIGHT_METERS = Units.inchesToMeters(28);
+    final double TARGET_HEIGHT_METERS = Units.inchesToMeters(105);
+    // Angle between horizontal and the camera.
+    final double CAMERA_PITCH_RADIANS = Units.degreesToRadians(29.7);// 29.5
+
+    private double yawVal = 0;
+    private double pitchVal = 0;
+    private double areaVal = 0;
     private boolean hasTarget = false;
     private int targetID;
-    
+
     PhotonCamera camera = new PhotonCamera("Camera");
 
+    // Code in periodic should only be to get camera data, checking position and
+    // moving should be done every execute
+    // Get data from camera
+    // To put camera to screen, there is an option in smart dashboard to add
     @Override
     public void periodic() {
         // This method will be called once per scheduler run
@@ -30,18 +40,35 @@ public class PoseEstimatorSubsytem extends SubsystemBase {
         if (result.hasTargets()) {
             this.yawVal = result.getBestTarget().getYaw();
             this.pitchVal = result.getBestTarget().getPitch();
-            this.skewVal = result.getBestTarget().getSkew();
             this.areaVal = result.getBestTarget().getArea();
-            this.hasTarget =true;
+            this.hasTarget = true;
             this.targetID = result.getBestTarget().getFiducialId();
-       
+
             SmartDashboard.putNumber("Yaw Value", yawVal);
             SmartDashboard.putNumber("Pitch Value", pitchVal);
             SmartDashboard.putNumber("Area Value", areaVal);
-        }
-        else{
+            SmartDashboard.putBoolean("Has a Target", hasTarget);
+            SmartDashboard.putNumber("Target ID: ", targetID);
+
+        } else {
             this.hasTarget = false;
         }
+    }
+
+    public double getRange() {
+        double range = PhotonUtils.calculateDistanceToTargetMeters(
+                CAMERA_HEIGHT_METERS,
+                TARGET_HEIGHT_METERS,
+                CAMERA_PITCH_RADIANS,
+                Units.degreesToRadians(pitchVal)
+        );
+
+        double rangeInInches = Units.metersToInches(range);
+
+        SmartDashboard.putNumber("Camera Distance", rangeInInches);
+
+        return range;
+        
     }
 
 }
